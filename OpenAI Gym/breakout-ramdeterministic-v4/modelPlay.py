10339c9cb45d9a24bc471e2337e26f5f4c6da5b7
wantyouring <pwc99@naver.com>
2019-04-29 10:54:35 +0900

    19.04.29
    breakout-ramdeterministic-v4환경 학습 잘 됨. lr이 문제였음. lr줄이면 기존 breakout-ram-v0에서도 학습 잘 되는지 확인하기. 실패 패널티도 추가.

---
+++ OpenAI Gym/breakout-ramdeterministic-v4/modelPlay.py
@@ -0,0 +1,100 @@
+# 학습모델 play. random action과 학습model 비교.
+
+import gym
+import pylab
+import numpy as np
+from doubleDQN2 import DoubleDQNAgent
+
+EPISODES = 20 # 처음은 random으로 수행, 나중에는 학습model로 수행
+global_step = 0
+
+def change_action(action):
+    if action == 0:
+        return 0
+    elif action == 1:
+        return 2
+    elif action == 2:
+        return 3
+    elif action == 3:
+        return 3
+
+if __name__ == "__main__":
+    env = gym.make('Breakout-ramDeterministic-v4')
+    state_size = 128
+    action_size = 3
+
+    agent = DoubleDQNAgent(state_size, action_size)
+    agent.load_model()
+    agent.epsilon = -1 # Q value에 의해서만 움직이게끔.
+    agent.render = True
+
+    scores, episodes = [], []
+    random_success_cnt = 0
+    model_success_cnt = 0
+    # 랜덤액션 진행시
+    for e in range(EPISODES):
+        done = False
+        score = 0
+        life = 5
+        env.reset()
+        for i in range(5):
+            env.step(1) # 시작 action.
+
+        while not done:
+            action = env.action_space.sample()
+            _, reward, done, info = env.step(change_action(action))
+            score += reward
+
+            if done:
+                if score > 0:
+                    random_success_cnt += 1
+                print("episode:", e, "  score:", score)
+                scores.append(score)
+                episodes.append(e)
+
+            if life != info['ale.lives']:
+                life = info['ale.lives']
+                for i in range(5):
+                    state, _, _, _ = env.step(1)
+                state = np.reshape(state, [1, 128])
+
+    # 학습모델 play
+    for e in range(EPISODES,EPISODES*2):
+        done = False
+        life = 5
+        score = 0
+        state = env.reset()
+        for i in range(5):
+            state, _, _, _ = env.step(1) # 시작 action.
+        state = np.reshape(state,[1,128])
+
+        while not done:
+            global_step += 1
+            if agent.render:
+                env.render()
+
+            # 현재 s에서 a취해 s`, r, done 정보 얻기.
+            action = agent.get_action(state)
+            next_state, reward, done, info = env.step(change_action(action))
+            score += reward
+            state = next_state
+            state = np.reshape(state, [1, 128])
+
+            if done:
+                if score > 0 :
+                    model_success_cnt += 1
+                print("episode:", e, "  score:", score)
+                scores.append(score)
+                episodes.append(e)
+                if e % 5 == 0:
+                    pylab.plot(episodes, scores)
+                    pylab.savefig("./play_score.png")
+
+            if life != info['ale.lives']:
+                life = info['ale.lives']
+                for i in range(5):
+                    state, _, _, _ = env.step(1)
+                state = np.reshape(state, [1, 128])
+
+    print("random : {}/{} success. rate : {}".format(random_success_cnt,EPISODES,random_success_cnt/EPISODES))
+    print("model : {}/{} success. rate : {}".format(model_success_cnt,EPISODES,model_success_cnt/EPISODES))
