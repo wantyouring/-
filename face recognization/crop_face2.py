12c47ff4b843ca67fc23196a4af1c895e08087b8
wantyouring <pwc99@naver.com>
2019-06-07 10:54:40 +0900

    mv folder

--- face recognization/crop_face2.py
+++
@@ -1,9 +0,0 @@
-import face_recognition
-import cv2
-
-image = face_recognition.load_image_file("img.jpg")
-face_locations = face_recognition.face_locations(image) # 얼굴 위치들 찾기
-face_landmarks_list = face_recognition.face_landmarks(image) # 눈, 코, 입, 턱 윤곽 잡음
-
-print(face_locations)
-print(face_landmarks_list)
